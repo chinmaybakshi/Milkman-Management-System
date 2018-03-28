caa1cb6285db9053fcb92305b350129df52cfa92
Chinmay <chinmayanandbakshi@gmail.com>
2018-03-28 09:26:57 +0530

    Data Files

---
+++ main.php
@@ -0,0 +1,47 @@
+<?php
+include("auth.php") ; //include auth.php file on all secure pages ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Welcome Home</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="form">
+<p>Welcome <?php echo $_SESSION['username']; ?>!</p>
+<p>This is secure area.</p>
+<p><a href="dashboard.php">Dashboard</a></p>
+<a href="logout.php">Logout</a>
+<?php
+    $servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "register";
+    
+    // Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$sql = "SELECT id, firstname, lastname FROM customers";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>Name</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["id"]."</td><td>".$row["firstname"]." ".$row["lastname"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+?>
+
+<br /><br /><br /><br />
+
+</div>
+</body>
+</html>
